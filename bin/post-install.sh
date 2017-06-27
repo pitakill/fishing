4f116ab5bfaca073d8178fb396c2e384c5246c2f
Lepoldo Caballero <git@pitakill.net>
2017-06-27 13:41:01 -0500

    Add Flow

---
+++ bin/post-install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+verifyExists() {
+  if ! [ -L $1 ]; then
+    case $1 in
+      @)
+        ln -s $PWD/src ln -s $PWD/node_modules/@
+        ;;
+      Components)
+        ln -s $PWD/src/components $PWD/node_modules/Components
+        ;;
+      $PWD/node_modules/Helpers)
+        ln -s $PWD/src/helpers $PWD/node_modules/Helpers
+        ;;
+      *)
+        echo "I don't know where points: $1"
+        ;;
+    esac
+  fi
+}
+
+symlinks=(@ Components Helpers)
+
+for i in ${symlinks[@]}; do
+  verifyExists $PWD/node_modules/$i
+done
